c3fc66feafa3a5d54c7a1c8d63a4e7da247306f7
Tri-Hartono <trihartono293@gmail.com>
2023-03-27 02:16:11 +0700

    first

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      height: {
+        140: '148vh',
+        150: '150vh',
+      },
+      width: {
+        94: '94%',
+        98: '98%',
+      },
+      fontFamily: {
+        sora: ['sora'],
+        poppins: ['poppins'],
+      },
+    },
+  },
+  plugins: [],
+};
